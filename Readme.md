a8a66e18f58144b3d73eee605fd826ea3fe01b02
Amartya Mondal <atm1504.in@gmail.com>
2020-07-07 01:46:21 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# atmdiary-backend
+
+atmdiary is a flutter based app to store your notes! The app is being developed by atm1504 for production level use! The app is a solution to give the Indians, nay the world and alternate app for the existing Chinese apps.
+
+### Backend
+The backend is being developed using NodeJs and MongoDb. Its currently under development.
+
+### How to setup in your system?
+* First, fork and clone the repository.
+* Create a virtual environment inside the cloned folder.
+* Activate the virtual environment.
+* Install the required dependencies by using the following command ```pip install -r requirements.txt```
+* Put your mongodb url in the ```atmdiary/config.py``` file.
+* Now execute ```python3 app.py``` to run the application.
---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+var config = require('./config.json');
+const app = express();
+app.use(express.json());
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+    console.log("Request received");
+    return res.status(200).json({"ok":"ok"})
+})
+
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(result => {
+      console.log('Connected Database');
+      app.listen(3000, function() {
+        console.log('App listening on port ' + 3000 + '!');
+      });
+    })
+    .catch(err => {
+      console.log(err);
+
+    });
