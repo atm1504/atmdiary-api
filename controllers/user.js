bcbb745ebce66a7addf84863fea52f479675f4cc
Amartya Mondal <atm1504.in@gmail.com>
2020-07-07 03:10:23 +0530

    feat: Add signup middleware

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,23 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const User = require("../models/user");
 
-exports.signup = (req, res, net)=>{
+exports.signup = (req, res, net) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).json({
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword
+      }
+    });
+  }
+
     return res.status(200).json({
                 status: 202,
                 message: "Success."})
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,43 @@
 const path = require('path');
 const express = require('express');
-const { body } = require('express-validator');
+const { body, check } = require('express-validator');
 const router = express.Router();
-const userController = require("./../controllers/user.js");
+const userController = require('./../controllers/user.js');
 
-module.exports = router.get("/signup", userController.signup);
+module.exports = router.get(
+	'/signup',
+	[
+		check('email')
+			.isEmail()
+			.withMessage('Please enter a valid email')
+			.custom((value, { req }) => {
+				return User.findOne({
+					email: value
+				}).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject('E-Mail exists already, please pick a different one.');
+					}
+				});
+			})
+			.normalizeEmail(),
+		body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
+			.isLength({
+				min: 5
+			})
+			.isAlphanumeric()
+			.trim(),
+		body('confirmPassword').trim().custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error('Passwords have to match!');
+			}
+			return true;
+		}),
+		body("phone").trim().custom((value, { req }) => {
+			if (value.length != "f" || value != "m") {
+				throw new Error('Incorrect gender value');
+			}
+			return true;
+		})
+	],
+	userController.signup
+);
